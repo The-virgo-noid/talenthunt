6dc8fe8fbadeb60c466eaa32d9df2fd1f033da49
Saurabh Katkar <saurabh.katkar102@gmail.com>
2020-07-04 20:38:34 +0530

    added stack and elements in world.dart file

--- lib/drawer_pages/world.dart
+++ lib/drawer_pages/world.dart
@@ -26,10 +26,64 @@ class _WorldState extends State<World> {
           ),
         ],
       ),
-
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            height: MediaQuery.of(context).size.height,
+            right: 10.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  Icons.more_horiz,
+                  size: 45.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.account_circle,
+                      size: 45.0,
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      size: 45.0,
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Icon(
+                      Icons.message,
+                      size: 45.0,
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Icon(
+                      Icons.share,
+                      size: 45.0,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+              ],
+            ),
+          ),
+          Positioned.directional(
+            textDirection: TextDirection.ltr,
+            bottom: 10.0,
+            start: 20.0,
+            child: Text(
+              '@name',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ],
       ),
     );
   }
